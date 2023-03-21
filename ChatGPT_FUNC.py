883002bd18732d65c4cf0b70ed041d610abaff61
gerolia90 <128107406+gerolia90@users.noreply.github.com>
2023-03-21 16:10:02 +0300

    Update ChatGPT_FUNC.py

--- ChatGPT_FUNC.py
+++ ChatGPT_FUNC.py
@@ -1,7 +1,7 @@
 import openai
 
 def chatgpt_func(prompt):
-    openai.api_key = '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'
+    openai.api_key = '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'
     completion = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
